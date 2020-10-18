0e856de925ad9bd52cdbf9636df4bc832a87b647
Minji Kim <minji1004y@gmail.com>
2020-10-18 14:29:17 +0900

    1018

--- pantone/src/App.js
+++ pantone/src/App.js
@@ -28,34 +28,22 @@ class App extends Component {
     } = this;
 
     return (
-      <Pantone 
-        color={color} 
-        palette={
-          <Palette colors={colors} selected={color} onSelect={handleColor} />
-        }
+      <body>
+        <Pantone 
+          color={color} 
+          palette={
+            <Palette colors={colors} selected={color} onSelect={handleColor} />
+          }
 
-        colorbox={
-          <ColorBox color={color}/>
-        }>
-        
-      </Pantone>
+          colorbox={
+            <ColorBox color={color}/>
+          }>
+          
+        </Pantone>
+      </body>
     )
   }
 }
 
 export default App;
 
-{/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
