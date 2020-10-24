21c01d7421f15529f8bf9e567e8ce6dad284272c
Jenny Lee <jenny5546@naver.com>
2020-10-24 15:47:39 +0900

    민지 사랑행

--- pantone/src/App.js
+++ pantone/src/App.js
@@ -1,49 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import useMousePosition from './useMousePosition';
 import Pantone from './components/Pantone';
 import ColorBox from "./components/Pantone";
 import Palette from "./components/Palette";
+import frame from './frame.png';
 
 const colors = [ "#92A8D1", "#88B04B", "#5F4B8B", "#FF6F61","#F7CAC9","#955251","#B163A3","#009473","#DD4124","#D94F70","#45B5AA","#F0C05A","#5A5B9F","#9B1B30","#DECDBE","#53B0AE"];
-// const pantonecolors = [ "#92A8D1", "#88B04B", "#5F4B8B", "#FF6F61","#F7CAC9","#955251","#B163A3","#009473","#DD4124"];
+const pantonecolors = [ "#92A8D1", "#88B04B", "#5F4B8B", "#FF6F61","#F7CAC9","#955251","#B163A3","#009473","#DD4124"];
 
-class App extends Component {
+const App = () => {
 
-  state = {
-    color: "#PANTONE COLOR",
+  const [color, setColor] = useState("#PANTONE COLOR");
+
+  const handleColor = (color) => {
+    setColor(color);
   };
 
-  handleColor = (color) => {
-    this.setState({
-      color,
-    });
-  };
+  const { x, y } = useMousePosition();
 
+  useEffect(()=>{
+    const frameImg = document.getElementById('frame-img');
+    frameImg.style.position = 'absolute';
+    frameImg.style.top = (y-250)+'px';
+    frameImg.style.left = (x-280)+'px';
+
+  },[x,y])
+
+  return (
+    <body>
+      <img className="frame" id="frame-img" src={frame} />
+      <Pantone 
+        color={color} 
+        palette={
+          <Palette colors={colors} selected={color} onSelect={handleColor} />
+        }
+        colorbox={
+          <ColorBox color={color}/>
+        }>
+      </Pantone>
+    </body>
+  )
   
-  render() {
-
-    const { color } = this.state;
-    const {
-      handleColor,
-    } = this;
-
-    return (
-      <body>
-        <Pantone 
-          color={color} 
-          palette={
-            <Palette colors={colors} selected={color} onSelect={handleColor} />
-          }
-
-          colorbox={
-            <ColorBox color={color}/>
-          }>
-          
-        </Pantone>
-      </body>
-    )
-  }
 }
 
 export default App;
-
